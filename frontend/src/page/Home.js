5f231681b4dbe2a28dd2a7aeff190765b02f770f
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-16 20:09:06 +0530

    Create Home.js

---
+++ frontend/src/page/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Home(){
+  return (
+    <div>
+      <h1>PASIYA Discover</h1>
+      <p> සිංහලෙන් ගුණාත්මක පාඩම් — Student friendly LMS</p>
+    </div>
+  );
+}
