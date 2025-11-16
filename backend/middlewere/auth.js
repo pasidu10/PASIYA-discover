214f645d264464be11ba4412cfb499c8ce653a35
pasidu10 <punnyanamda.thero@gmail.com>
2025-11-16 19:59:04 +0530

    Create auth.js

---
+++ backend/middlewere/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ msg: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // { userId, iat, exp }
+    next();
+  } catch (err) {
+    return res.status(401).json({ msg: 'Invalid token' });
+  }
+};
